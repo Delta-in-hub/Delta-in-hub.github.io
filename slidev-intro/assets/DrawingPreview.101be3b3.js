aedc349ba4b6d946dd3ba24a4a0a6f3c5c260eff
Delta <DeltaMail@qq.com>
2022-12-02 00:17:00 +0800

    update slidev intro

--- slidev-intro/assets/DrawingPreview.101be3b3.js
+++
@@ -1,1 +0,0 @@
-import"./index.07cf733b.js";import{am as r}from"./index.07cf733b.js";export{r as default};
---
+++ slidev-intro/assets/DrawingPreview.d4af7a47.js
@@ -0,0 +1,1 @@
+import"./index.8cbb9549.js";import{am as r}from"./index.8cbb9549.js";export{r as default};
